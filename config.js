18e9695be803e2724064e13ff0423f76c5141f19
taufik234 <48504038+taufik234@users.noreply.github.com>
2023-04-30 21:50:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_KEY_RM_BG: "bgNyncqNaYHeycdQKME8u9FE",
-  API_KEY_OPEN_AI: "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",
+  API_KEY_RM_BG: "",
+  API_KEY_OPEN_AI: "",
 };
